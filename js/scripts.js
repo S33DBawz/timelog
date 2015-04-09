6a4ab52cf2f7e8c222f3b4c85ff44b4b5c5b02cd
s33dbawz <albert.dolsoe@gmail.com>
2015-04-09 15:46:20 +0200

    more changes

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,20 @@ function CalculateHours() {
 		endHour = $('#end').val();
 
 	if (startHour != "" && endHour != "") {
+
+		if (startHour.indexOf(':')) {
+			startHour = startHour.replace(":", ".");
+		}
+		if (endHour.indexOf(':')) {
+			endHour = endHour.replace(":", ".");
+		}
+		if (startHour.indexOf(',')) {
+			startHour = startHour.replace(",", ".");
+		}
+		if (endHour.indexOf(',')) {
+			endHour = endHour.replace(",", ".");
+		}
+
 		var workHours = endHour - startHour - 0.50,
 			workHoursMath = (Math.round(workHours * 100) / 100),
 			result = (workHoursMath).toFixed(1);
